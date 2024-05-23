0e38359075eb2ac8d8bd4f0b653363e82982e077
Tek052 <71016586+Tek052@users.noreply.github.com>
2024-05-23 10:59:07 +0100

    Add files via upload

---
+++ Assignment examp[le/Assignment examp[le/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Assignment_examp_le
+{
+   
+        class Loop
+        {
+            public static void Main(string[] args)
+            {
+            int[] variabile = new int[] { 1, 2, 3 };
+
+            foreach (int b in variabile)
+            {
+                System.Console.WriteLine(b);
+            }
+        }
+        }
+    }
+
