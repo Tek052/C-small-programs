e05b422a93b4ef52232f849092304d73b433456e
Tek052 <71016586+Tek052@users.noreply.github.com>
2024-05-23 10:58:14 +0100

    Add files via upload

---
+++ Accurancy/Accurancy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Accurancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            double result1 = 0.1 * 8;
+            double result2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+            Console.WriteLine($"1st result is{result1:R}");
+            Console.WriteLine($"2nd result is{result2:R}");
+
+            double difference = result1 - result2;
+            Console.WriteLine($"The difference betweeen is {difference:R}");
+
+             
+    }
+}
